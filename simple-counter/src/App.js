57c60fecc6dcce0f8e945db016bbd5797ea28f4b
akamamara <aldi_okta10@apps.ipb.ac.id>
2020-11-23 10:26:42 +0700

    change directory

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Name(){
+  return(
+    <div>
+      <p>Halo</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Name />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
